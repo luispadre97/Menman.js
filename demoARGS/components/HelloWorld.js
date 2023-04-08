4ec04f46b5c66d41a6b36976e9632d870d3931a6
luispadre123 <119064227+luispadre123@users.noreply.github.com>
2023-04-08 16:35:37 -0600

    add vite

--- demoARGS/components/HelloWorld.js
+++ demoARGS/components/HelloWorld.js
@@ -1,3 +1,3 @@
 export default function HelloWorld() {
-    return (<h1>Hola mundo</h1>);
+    return ('<h1>Hola mundo</h1>');
 }
--- demoARGS/main.js
+++ demoARGS/main.js
@@ -1,5 +1,5 @@
 import { createApp } from './../src/index.js';
-import HelloWorld from './components/HelloWorld.menman';
+import HelloWorld from './components/HelloWorld.js';
 
 const app = createApp(HelloWorld);
 app.mount('#app');
